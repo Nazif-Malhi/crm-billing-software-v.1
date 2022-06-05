3275bc93a3b0cc892e140cfbfa4baacee663d6b4
Adnan913 <adnansami9134@gmail.com>
2022-06-04 23:23:09 -0700

    Add print btn to print react table component

--- src/Application/Pages/Inventory/Category/CategoryTable.jsx
+++ src/Application/Pages/Inventory/Category/CategoryTable.jsx
@@ -12,6 +12,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+
 function DropDown() {
   const [action, setAction] = React.useState('');
 
